8dcba64ea828acd6c6c0e67da6b439c71a29edff
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-03-04 08:18:59 -0300

    feat: add new workspace

--- first-iac-project/main.tf
+++ first-iac-project/main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rocketseat-bucket-iac-vinicius"
+  bucket = "rocketseat-bucket-iac-vinicius-${terraform.workspace}"
 
   tags = {
     Name = "First bucket"
     Iac = true
+    context = "${terraform.workspace}"
   }
 }
