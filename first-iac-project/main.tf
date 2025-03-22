396ca8985fd36a452636fb94a4f60290f9179793
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-03-22 08:25:20 -0300

    feat: add external module sqs

--- first-iac-project/main.tf
+++ first-iac-project/main.tf
@@ -17,3 +17,15 @@ module "cloudfront" {
     module.s3
   ]
 }
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+
+  name = "example"
+
+  create_dlq = true
+
+  tags = {
+    Iac = true
+  }
+}
