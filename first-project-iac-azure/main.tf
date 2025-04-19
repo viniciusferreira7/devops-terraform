ce604e35294305adfec6feba31cea43a4e59393b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-04-19 08:41:25 -0300

    feat: create azure resources

--- first-project-iac-azure/main.tf
+++ first-project-iac-azure/main.tf
@@ -1,7 +1,17 @@
 resource "azurerm_resource_group" "rg" {
-  name = "rocketseat-hmg"
+  name     = "rocketseat-hmg"
   location = "Brazil south"
+  lifecycle {
+    prevent_destroy = true
+  }
   tags = {
     Iac = "True"
   }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                = "rocketseat-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
 }
--- first-project-iac-azure/providers.tf
+++ first-project-iac-azure/providers.tf
@@ -1,12 +1,17 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.26.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  skip_provider_registration = true
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
 }
